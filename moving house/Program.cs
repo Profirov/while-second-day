0d235d71b6f89135292f0a614eb409fb02b2678f
user <user@DESKTOP-Q7SRQOE>
2023-11-08 09:32:45 +0200

    Add project files.

---
+++ moving house/Program.cs
@@ -0,0 +1,20 @@
+﻿int width = int.Parse(Console.ReadLine());
+int lenght = int.Parse(Console.ReadLine());
+int height = int.Parse(Console.ReadLine());
+int totalVolume = width * height * lenght;   
+string input = Console.ReadLine();
+while (input != "Done")
+{
+    int boxes = int.Parse(input);
+    totalVolume -= boxes;
+    if (totalVolume < 0)
+    {
+        Console.WriteLine($"No more free space! You need {Math.Abs(totalVolume)} Cubic meters more.");
+        break;
+    }
+    input = Console.ReadLine();
+}
+if (totalVolume > 0)
+{
+    Console.WriteLine($"{totalVolume} Cubic meters left.");
+}
---
+++ while second day/Program.cs
@@ -0,0 +1,31 @@
+﻿int cakewidth = int.Parse(Console.ReadLine());
+int cakeLength = int.Parse(Console.ReadLine());
+
+int pieceTaken = 0;
+bool iscAkeOver = false;
+int cakeSize = cakewidth * cakeLength;
+
+string input = Console.ReadLine();
+while (input != "Stop")
+{ 
+  int piece = int.Parse(input);
+    pieceTaken += piece;
+
+    if (pieceTaken >= cakeSize)
+    { 
+      int neededPieces = pieceTaken - cakeSize;
+
+        iscAkeOver = true;
+
+        Console.WriteLine($"No more cake left! You need {neededPieces} pieces more.");
+        break;
+    }
+    input = Console.ReadLine();
+}
+
+
+int piecesLeft = cakeSize - pieceTaken;
+if (piecesLeft > 0)
+{
+    Console.WriteLine($"{piecesLeft} pieces left.");
+}
